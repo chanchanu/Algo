d47e1404633d98d7cd994cb129886697a2c6a39a
chan-dataa <cksdn514@gmail.com>
2021-01-09 21:19:01 +0900

    # 자료형에 관한 연습.py

---
+++ practice.py
@@ -0,0 +1,6 @@
+#a = input() # input()의 기본 자료형은 str이다
+#a = int(input()) # input()을 int자료형을 변환해서 저장
+#a = input().split() # 띄어쓰기 단위
+#a,b,c = map(int,input().split()) # 띄어쓰기 단위에서 int자료형으로 바꿔서 각 변수에 넣기
+a = [int(i) for i in input().split()] # 띄어쓰기 단위에서 int 자료형으로 바꿔서 list로 저장 
+# []는 list, ()는 튜플
