fe0c17edba5b55046bdc1f0234a1d294e55ca607
chan-dataa <cksdn514@gmail.com>
2021-01-12 07:32:18 +0900

    # 피보나치.py

---
+++ 재귀/problem2.py
@@ -0,0 +1,13 @@
+# 피보나치 
+def fib(n):
+    result = 0
+    if(n == 0):
+        result = 0
+    elif(n == 1):
+        result = 1
+    else:
+        result = fib(n-1) + fib(n-2)
+    return result
+
+n = int(input())
+print(fib(n))
